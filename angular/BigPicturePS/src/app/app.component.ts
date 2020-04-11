dd6aeae3dfb865698dd95e689790c7d0bcc9fad9
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-11 20:50:35 +0530

    Created app component for inventory

--- angular/BigPicturePS/src/app/app.component.ts
+++ angular/BigPicturePS/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'angventory-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
+  `,
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle = 'Angventory'; // property/data followed by :datatype
 }
