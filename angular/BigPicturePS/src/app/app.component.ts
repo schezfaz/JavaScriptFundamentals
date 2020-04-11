1cf0932ae6f738c0ef7d32a13f63c3c988feecb7
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-12 05:25:43 +0530

    added if and for conditions for products

--- angular/BigPicturePS/src/app/app.component.ts
+++ angular/BigPicturePS/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'pm-root',
   template: `
     <div>
-      <h1>{{pageTitle}}</h1>
+      <h1 style="text-align:center;">{{pageTitle}}</h1>
       <pm-products></pm-products>
     </div>
   `,
