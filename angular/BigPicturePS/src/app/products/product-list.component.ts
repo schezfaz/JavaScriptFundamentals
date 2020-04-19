37d3dedf14d3a1e024c3a9a0a4c3c167591709f9
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-19 20:51:54 +0530

    added OnInit lifecycle method

--- angular/BigPicturePS/src/app/products/product-list.component.ts
+++ angular/BigPicturePS/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from './product';
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     showImage: boolean = false;
     listFilter: string = 'Product';
@@ -37,5 +37,9 @@ export class ProductListComponent{
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void{
+        console.log("'inside oninit method");
     }
 }
