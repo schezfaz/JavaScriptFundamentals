940b2c1d27100e8ec1be7c94fac298b9caa332c0
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-04-24 00:53:55 +0530

    created module for star ratings -static

---
+++ angular/BigPicturePS/src/app/shared/star.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+    selector: 'pm-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges{
+    rating: number = 4;
+    starWidth: number;
+
+    ngOnChanges(): void{
+        this.starWidth = this.rating*12;
+    }
+}
